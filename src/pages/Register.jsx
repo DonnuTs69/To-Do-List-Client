e95f909359d05770633858fc1ec579825ac64d16
DonnuTs69 <febrykurniawanhakim@gmail.com>
2023-04-09 22:38:49 +0700

    create: register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,61 @@
-import { Box, Container, Paper, Typography } from "@mui/material"
-import React from "react"
+import { Box, Button, Container, Input, Paper, Typography } from "@mui/material"
+import React, { useState } from "react"
+import { axiosInstance } from "../api"
 
 const Register = () => {
+  const [emailInput, setEmailInput] = useState("")
+  const [usernameInput, setUsernameInput] = useState("")
+  const [passwordInput, setPasswordInput] = useState("")
+
+  const registerUser = async (e) => {
+    e.preventDefault()
+    try {
+      await axiosInstance.post("/auth/register", {
+        username: usernameInput,
+        email: emailInput,
+        password: passwordInput,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <>
-      <Box>
-        <Typography>Register</Typography>
+      <Box
+        top="100px"
+        display="grid"
+        margin="auto"
+        justifyContent="center"
+        position="relative"
+        sx={{ width: "500px" }}
+        border="1px solid red"
+      >
+        <Typography variant="h3" gutterBottom>
+          Register
+        </Typography>
+        <form onSubmit={registerUser}>
+          <Box>
+            <Typography>Username</Typography>
+            <Input
+              value={usernameInput}
+              onChange={(e) => setUsernameInput(e.target.value)}
+            />
+            <Typography>Email</Typography>
+            <Input
+              value={emailInput}
+              onChange={(e) => setEmailInput(e.target.value)}
+              type="email"
+            />
+            <Typography>Password</Typography>
+            <Input
+              type="password"
+              value={passwordInput}
+              onChange={(e) => setPasswordInput(e.target.value)}
+            />
+          </Box>
+          <Button type="submit">Submit</Button>
+        </form>
       </Box>
     </>
   )
