e24d64e3cd839adb1e33326dd854d177bc4cb3a2
DonnuTs69 <febrykurniawanhakim@gmail.com>
2023-04-12 15:57:29 +0700

    create: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,57 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Button, Input, Typography } from "@mui/material"
+import { useState } from "react"
+import { axiosInstance } from "../api"
 
 const Login = () => {
+  const [usernameOrEmail, setUsernameOremail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const loginUser = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axiosInstance.post("/auth/login", {
+        usernameOrEmail: usernameOrEmail,
+        password: password,
+      })
+
+      console.log(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
-    <Box>
-      <Typography>Login</Typography>
-    </Box>
+    <>
+      <Box
+        top="100px"
+        display="grid"
+        margin="auto"
+        justifyContent="center"
+        position="relative"
+        sx={{ width: "500px" }}
+        border="1px solid red"
+      >
+        <Typography variant="h3" gutterBottom>
+          Login
+        </Typography>
+        <form onSubmit={loginUser}>
+          <Box>
+            <Typography>Username or Email</Typography>
+            <Input
+              value={usernameOrEmail}
+              onChange={(e) => setUsernameOremail(e.target.value)}
+              type="text"
+            />
+            <Typography>Password</Typography>
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Box>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Box>
+    </>
   )
 }
 
